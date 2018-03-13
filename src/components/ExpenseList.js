c1c0662511704b9cca0235b4a9370bb800d27ceb
Ron San Jose <ronsj1@gmail.com>
2018-03-12 20:57:50 -0700

    Add expenses selector and ExpenseListItem SFC

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => (
   <div>
     <h1>ExpenseList</h1>
-    {props.filters.text}
-    {props.expenses.length}
+    {props.expenses.map((expense) => (
+      <ExpenseListItem key={expense.id} {...expense} />
+    ))}
   </div>
 );
 
 const mapStateToProps = ({ expenses, filters }) => ({
-  expenses,
-  filters
+  expenses: selectExpenses(expenses, filters)
 });
 
 export default connect(mapStateToProps)(ExpenseList);
