f66407ba6e054774214191a4cebe3e19ae7e3a0d
Ron San Jose <ronsj1@gmail.com>
2018-03-18 17:10:34 -0700

    Move 'remove' button to the Edit Expense page

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
-    <h3>Description: {description}</h3>
+    <Link to={`/edit/${id}`}>
+      <h3>Description: {description}</h3>
+    </Link>
     <p>Amount: {amount}</p>
     <p>Created At: {createdAt}</p>
-    <button onClick={() => {
-      dispatch(removeExpense({ id }))
-    }}>Remove</button>
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
